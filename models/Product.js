fd48779452e4cccb4de63790c244329bae0a4a6d
mlsfiles75@gmail.com <mlsfiles75@gmail.com>
2025-08-25 14:37:03 -0400

    added schema and model

---
+++ models/Product.js
@@ -0,0 +1,34 @@
+const mongoose = require ('mongoose')
+const {Schema} = mongoose
+
+const productSchema = new Schema({
+    name: {
+        type: String, required: [true, "Name"]
+    },
+    description: {
+        type: String, required: [true,  "Description"]
+    },
+    price: {
+        type: Number, required: [true, "Price"],
+        min: [0, 'Must be greater than zero']
+    },
+    category: {
+        type: String, required: [true, "Category"]
+    },
+    inStock: {
+        type: Boolean,
+        default: true
+    },
+    tags: {
+        type: [String]
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    
+})
+
+const Product = mongoose.model('Product', productSchema)
+module.exports = Product
+
